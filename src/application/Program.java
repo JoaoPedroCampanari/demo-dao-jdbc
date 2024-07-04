a94e4a6255dcc752fb9b505ca09a05a341b90bf9
Joaofcampanari <joaofcampanari@gmail.com>
2024-07-04 10:00:37 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Program {
     public static void main(String[] args) {
 
+
+
         Department obj = new Department(1,"Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(1, "Joao", "Joao@gmail.com", LocalDate.parse("03/02/2004",Seller.formatter), 6000.0, obj);
+
+        System.out.println(seller);
 
     }
 }
