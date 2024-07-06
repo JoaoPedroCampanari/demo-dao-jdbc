a0e4a80592a3fd9bed95e2f44cc8d2631cf2815a
Joaofcampanari <joaofcampanari@gmail.com>
2024-07-06 09:43:25 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,10 +8,12 @@ import model.entities.Seller;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSallerDao();
 
@@ -48,5 +50,16 @@ public class Program {
         sellerDao.update(seller);
 
         System.out.println("Update Completed!");
+
+        System.out.println("\n=== TEST 6: seller delete =====");
+        System.out.println("Enter id for delete teste: ");
+        int id = sc.nextInt();
+
+        sellerDao.deleteById(id);
+
+        System.out.println("Delete completed");
+
+        sc.close();
+
     }
 }
